1c207a142f6b7d3c6304a68877fe7cb477b7cde5
Sarah Carolina J. Silva <sarah.cjs@hotmail.com>
2016-05-15 20:23:31 -0300

    Acompanhamento dos videos
    Codigos feitos como teste e acompanhamento enquanto assistia as aulas

---
+++ Python para Zumbis/zumbi10.py
@@ -0,0 +1,5 @@
+a = 'abacate'
+b = ' e banana'
+print(a+b)
+def soma(x,y):
+    print(x+y)
---
+++ Python para Zumbis/zumbi20.py
@@ -0,0 +1,6 @@
+nome = input ('Digite o seu nome: ')
+print (nome)
+
+n1 = int(input ('Digite o primeiro número: '))
+n2 = int(input ('Digite o segundo número: '))
+print (n1 + n2)
